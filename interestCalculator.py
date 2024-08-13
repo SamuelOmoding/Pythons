b68cf53415574e40cb1a95186c68c5a2690639ec
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-08-13 23:02:30 +0300

    Interest

---
+++ interestCalculator.py
@@ -0,0 +1,44 @@
+# Python compound interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+# while principle <= 0:
+#     principle = float(input("Enter the principal amount (positive number): "))
+#     if principle <= 0:
+#         print("Principle can't be less than or equal to zero")
+
+# while rate <= 0:
+#     rate = float(input("Enter the interest rate: "))
+#     if rate <= 0:
+#         print("Interest rate can't be less than or equal to zero")
+
+# while time <= 0:
+#     time = int(input("Enter the time in years: "))
+#     if time <= 0:
+#         print("Time can't be less than or equal to zero")
+while True:
+    principle = float(input("Enter the principal amount (positive number): "))
+    if principle <= 0:
+        print("Principle can't be less than to zero")
+    else:
+        break
+while True:
+    rate = float(input("Enter the interest rate: "))
+    if rate <= 0:
+        print("Interest rate can't be less than to zero")
+    else:
+        break
+while True:
+    time = int(input("Enter the time in years: "))
+    if time <= 0:
+        print("Time can't be less than to zero")
+    else:
+        break
+print(principle) 
+print(rate)
+print(time)   
+
+total = principle * pow((1 + rate / 100), time)
+print(f"Balance after {time} year/s: Ksh.{total:.2f}")
