3980a5d6d4ddc2e222d6b19ae07b5279a8b0dcfa
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-08-19 16:25:49 +0300

    guesses

--- numberGuess.py
+++ numberGuess.py
@@ -14,9 +14,9 @@ print(f"Select a number between {lowest_num} and {highest_num}")
 while is_running:
     guess = input("Enter your guess: ")
     
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
+    # if guess.isdigit():
+    #     guess = int(guess)
+    #     guesses += 1
         
         if guess < lowest_num or guess > highest_num:
             print("Number is out of range!")
