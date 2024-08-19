852d5c8cc5bace0099a14b6b3cc829d071d04770
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-08-19 16:21:29 +0300

    hangman

--- hangman.py
+++ hangman.py
@@ -3,17 +3,13 @@ from wordslist import words
 import random
 
 
-# Dictionary for hangman art
-hangman_art = {
-    0: (" ", " ", " "),
-    1: (" o ", " ", " "),
-    2: (" o ", " | ", " "),
-    3: (" o ", "/|", " "),
-    4: (" o ", "/|\\", " "),
-    5: (" o ", "/|\\", "/ "),
-    6: (" o ", "/|\\", "/ \\")
-}
 
+
+def display_man(wrong_guesses):
+    print("**************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("**************")
 
 def display_hint(hint):
     print(" ".join(hint))
