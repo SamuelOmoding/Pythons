de644a1775268d66cdf903edd79e5b8be58e83f9
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-08-19 16:26:09 +0300

    guesses

--- numberGuess.py
+++ numberGuess.py
@@ -8,15 +8,13 @@ answer = random.randint(lowest_num, highest_num)
 guesses = 0
 is_running = True
 
-print("Python Number Guessing Game")
-print(f"Select a number between {lowest_num} and {highest_num}")
 
 while is_running:
     guess = input("Enter your guess: ")
     
-    # if guess.isdigit():
-    #     guess = int(guess)
-    #     guesses += 1
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
         
         if guess < lowest_num or guess > highest_num:
             print("Number is out of range!")
